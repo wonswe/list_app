d9fe078b4d17048f77d008f182e10d0dc11ecf49
wonswe <64626054+wonswe@users.noreply.github.com>
2025-08-27 00:53:08 -0700

    test dom

---
+++ main.js
@@ -0,0 +1,5 @@
+const listContainer = document.querySelector('.list-container');
+const listItem = document.createElement('div');
+listItem.innerHTML = '<p>우유</p>';
+
+listContainer.appendChild(listItem);
